6abdcb7a26d742fd27c7632eef39df4595c4c193
Elaine Anderson <eparie@bu.edu>
2018-06-19 19:48:13 +0000

    skeleton

--- lib/ballroom_events/cli.rb
+++ lib/ballroom_events/cli.rb
@@ -1,8 +1,39 @@
-#CLI Controller 
+#CLI Controller
 class BallroomEvents::CLI
 
   def call
-    puts "Ballroom Events"
+    list_events
+    location
+    specific_event
   end
+
+
+def list_events
+  puts "Ballroom Events"
+  puts <<-DOC
+  1. event 1
+  2. event 2
+  DOC
+end
+
+def location
+  puts "Enter the name of the city you want to search or type 'all' to browse all events"
+  input = nil
+  while input != "all", case statement
+    input = gets.strip.downcase
+    case input
+  when input == "boston"
+    puts "Boston events"
+  when input == "new york"
+    puts "New York Events"
+  end 
+
+end
+
+def specific_event
+  puts "Enter the number of the event you want more information on."
+  input = gets.strip
+
+end
 
 end
