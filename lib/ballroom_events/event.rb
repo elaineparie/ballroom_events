85076a04824679a80a9f6b28b65593ac47cc836e
Elaine Anderson <eparie@bu.edu>
2018-06-25 21:52:41 +0000

    working

--- lib/ballroom_events/event.rb
+++ lib/ballroom_events/event.rb
@@ -1,21 +1,45 @@
 class BallroomEvents::Event
 
-  attr_accessor :name, :location, :contact_name, :contact_number, :contact_email, :url, :in, :tn, :mn, :fl, :ma, :tx, :ca, :nj, :ga, :md, :ut, :hi, :co, :ny, :az, :nc, :nv, :oh, :ct
-  attr_accessor :la, :pa, :ia, :wi, :nh, :va, :or, :pa, :wa, :mo, :sc, :ky, :nm, :ne, :pr, 
-  def self.all
-    #returns all events
+  attr_accessor :name, :location, :contact_name, :contact_number, :contact_email, :url
+  attr_accessor :in, :tn, :mn, :fl, :ma, :tx, :ca, :nj, :ga, :md, :ut, :hi, :co, :ny, :az, :nc, :nv, :oh, :ct
+  attr_accessor :la, :pa, :ia, :wi, :nh, :va, :or, :wa, :mo, :sc, :ky, :nm, :ne, :pr
 
-    event_1 = self.new
-    event_1.name = "name"
-    event_1.location = "location"
-    event_1.contact_name = "name"
-    event_1.contact_number = "number"
-    event_1.url = "url"
-    event_1.contact_email = "contact_email"
+@in = []
+@tn = []
+@mn = []
+@fl = []
+@ma = []
+@tx = []
+@ca = []
+@nj = []
+@ga = []
+@md = []
+@ut = []
+@hi = []
+@co = []
+@ny = []
+@az = []
+@nc = []
+@nv = []
+@oh = []
+@ct = []
+@la = []
+@pa = []
+@ia = []
+@wi = []
+@nh = []
+@va = []
+@or = []
+@wa = []
+@mo = []
+@sc = []
+@ky = []
+@nm = []
+@ne = []
+@pr = []
 
-    [event_1]
 
-  end
-
+def in_events
+end
 
 end
