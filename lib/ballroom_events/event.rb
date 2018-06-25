225293f16811c963d5a1f78be072c4ff148f03d6
Elaine Anderson <eparie@bu.edu>
2018-06-25 22:48:28 +0000

    working

--- lib/ballroom_events/event.rb
+++ lib/ballroom_events/event.rb
@@ -1,45 +1,19 @@
 class BallroomEvents::Event
 
   attr_accessor :name, :location, :contact_name, :contact_number, :contact_email, :url
-  attr_accessor :in, :tn, :mn, :fl, :ma, :tx, :ca, :nj, :ga, :md, :ut, :hi, :co, :ny, :az, :nc, :nv, :oh, :ct
-  attr_accessor :la, :pa, :ia, :wi, :nh, :va, :or, :wa, :mo, :sc, :ky, :nm, :ne, :pr
 
-@in = []
-@tn = []
-@mn = []
-@fl = []
-@ma = []
-@tx = []
-@ca = []
-@nj = []
-@ga = []
-@md = []
-@ut = []
-@hi = []
-@co = []
-@ny = []
-@az = []
-@nc = []
-@nv = []
-@oh = []
-@ct = []
-@la = []
-@pa = []
-@ia = []
-@wi = []
-@nh = []
-@va = []
-@or = []
-@wa = []
-@mo = []
-@sc = []
-@ky = []
-@nm = []
-@ne = []
-@pr = []
+  def self.all
+    #returns all events
 
+    event_1 = self.new
+    event_1.name = "name"
+    event_1.location = "location"
+    event_1.contact_name = "name"
+    event_1.contact_number = "number"
+    event_1.url = "url"
+    event_1.contact_email = "contact_email"
 
-def in_events
-end
+    [event_1]
 
+  end
 end
