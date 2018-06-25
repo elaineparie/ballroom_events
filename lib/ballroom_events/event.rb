f682e910b92f431dd8a58fd9f0c46800d7312916
Elaine Anderson <eparie@bu.edu>
2018-06-25 21:44:15 +0000

    working

--- lib/ballroom_events/event.rb
+++ lib/ballroom_events/event.rb
@@ -1,7 +1,7 @@
 class BallroomEvents::Event
 
-  attr_accessor :name, :location, :contact_name, :contact_number, :contact_email, :url
-
+  attr_accessor :name, :location, :contact_name, :contact_number, :contact_email, :url, :in, :tn, :mn, :fl, :ma, :tx, :ca, :nj, :ga, :md, :ut, :hi, :co, :ny, :az, :nc, :nv, :oh, :ct
+  attr_accessor :la, :pa, :ia, :wi, :nh, :va, :or, :pa, :wa, :mo, :sc, :ky, :nm, :ne, :pr, 
   def self.all
     #returns all events
 
@@ -16,4 +16,6 @@ class BallroomEvents::Event
     [event_1]
 
   end
+
+
 end
