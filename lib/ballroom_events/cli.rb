b0c52e63c0acf49d4409653d98b6468ae111656b
Elaine Anderson <eparie@bu.edu>
2018-07-10 01:35:22 +0000

    specific event displays event without scraping twice

--- lib/ballroom_events/cli.rb
+++ lib/ballroom_events/cli.rb
@@ -28,7 +28,10 @@ class BallroomEvents::CLI
     input = gets.strip.downcase
     if input.to_i > 0 && input.to_i < BallroomEvents::Event.all.length
       event = BallroomEvents::Event.all[input.to_i - 1]
-      if !event.contact_name
+      if event.contact_name
+        puts "Event Organizer: #{event.contact_name}, Organizer Phone: #{event.contact_number}, Organizer Email: #{event.contact_email}, Website: #{event.web}"
+        specific_event
+      elsif
       BallroomEvents::Scraper.scrape_event(event)
       puts "Event Organizer: #{event.contact_name}, Organizer Phone: #{event.contact_number}, Organizer Email: #{event.contact_email}, Website: #{event.web}"
      specific_event
